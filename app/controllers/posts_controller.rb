1c8f4eda2516a02a4962ad03da74835b1dadabaf
Ori Markowitz <ori.markowitz@gmail.com>
2020-12-14 18:39:48 -0500

    commit tree 67763b57e8f08d6d589a82b4af718db4b9ad416a

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,9 +10,21 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    @post.update(post_params)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
 
-    redirect_to post_path(@post)
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   private
